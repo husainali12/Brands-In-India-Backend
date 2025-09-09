57e27824f0bca1be28774ceae18a537978e242fd
Harmanjot Singh <narulasahil2002@gmail.com>
2025-09-09 15:22:59 +0530

    added-search-filter

--- service/authService.js
+++ service/authService.js
@@ -14,12 +14,22 @@ async function getUserById(id) {
 }
 
 const getUsers = async (req, res) => {
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, search = "" } = req.query;
   const pageNum = parseInt(page, 10) || 1;
   const limitNum = parseInt(limit, 10) || 10;
   const skip = (pageNum - 1) * limitNum;
+  let filter = {};
+  if (search) {
+    filter = {
+      $or: [
+        { name: { $regex: search, $options: "i" } }, // case-insensitive
+        { email: { $regex: search, $options: "i" } },
+        { phone: { $regex: search, $options: "i" } },
+      ],
+    };
+  }
   // const skip = (page - 1) * limit;
-  const users = await User.find({}).skip(skip).limit(limitNum);
+  const users = await User.find(filter).skip(skip).limit(limitNum).lean();
   const totalUsers = await User.countDocuments();
   const totalPages = Math.ceil(totalUsers / limitNum);
   return {
