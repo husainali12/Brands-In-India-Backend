9c3b6b20473c2041641c5010ef3d6772750db0a4
Harmanjot Singh <narulasahil2002@gmail.com>
2025-09-25 14:22:16 +0530

    fixed

--- routes/UpComingUserRoutes.js
+++ routes/UpComingUserRoutes.js
@@ -5,6 +5,6 @@ const {
 } = require("../controller/UpComingUserController");
 const firebaseAuth = require("../middleware/firebaseAuth");
 const router = express.Router();
-router.post("/create-upcoming-user", firebaseAuth("any"), createUpComingUser);
+router.post("/create-upcoming-user", createUpComingUser);
 router.get("/all-upcoming-users", firebaseAuth("admin"), getAllUpComingUsers);
 module.exports = router;
